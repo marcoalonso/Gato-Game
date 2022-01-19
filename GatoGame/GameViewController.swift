83220a06f112a39c2f997b0266a3cd7be773ce5f
marcoalonso <marcoalonsorota@hotmail.com>
2022-01-19 15:28:52 -0600

    Cajas-TapGesture-agregadas

--- GatoGame/GameViewController.swift
+++ GatoGame/GameViewController.swift
@@ -9,24 +9,57 @@ import UIKit
 
 class GameViewController: UIViewController {
 
+    @IBOutlet weak var nombreJugadorLbl: UILabel!
+    @IBOutlet weak var marcadorJugadorLbl: UILabel!
+    @IBOutlet weak var marcadorPcLbl: UILabel!
+    
+    
+    @IBOutlet weak var caja1: UIImageView!
+    @IBOutlet weak var caja2: UIImageView!
+    @IBOutlet weak var caja3: UIImageView!
+    @IBOutlet weak var caja4: UIImageView!
+    @IBOutlet weak var caja5: UIImageView!
+    @IBOutlet weak var caja6: UIImageView!
+    @IBOutlet weak var caja7: UIImageView!
+    @IBOutlet weak var caja8: UIImageView!
+    @IBOutlet weak var caja9: UIImageView!
+    
+    var nombreJugador: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        nombreJugadorLbl.text = nombreJugador ?? "Jugador 1" + ": "
+        
+        //Agregar la gestura a cada imagen
+        crearToque(on: caja1, type: .uno)
+        crearToque(on: caja2, type: .dos)
+        crearToque(on: caja3, type: .tres)
+        crearToque(on: caja4, type: .cuatro)
+        crearToque(on: caja5, type: .cinco)
+        crearToque(on: caja6, type: .seis)
+        crearToque(on: caja7, type: .siete)
+        crearToque(on: caja8, type: .ocho)
+        crearToque(on: caja9, type: .nueve)
+    }
+    
+    func crearToque(on imageView: UIImageView, type caja: Caja){
+        let tap = UITapGestureRecognizer(target: self, action: #selector(self.cajaClick(_:)))
+        tap.name = caja.rawValue
+        imageView.isUserInteractionEnabled = true
+        imageView.addGestureRecognizer(tap)
+    }
+    
+    @objc func cajaClick(_ sender: UITapGestureRecognizer){
+        print("Caja: \(sender.name) fue seleccionada")
     }
     
     @IBAction func regresarBtn(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    enum Caja: String {
+        case uno, dos, tres, cuatro, cinco, seis, siete, ocho, nueve
     }
-    */
 
 }
--- GatoGame/ViewController.swift
+++ GatoGame/ViewController.swift
@@ -9,11 +9,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nombre: UITextField!
+    @IBOutlet weak var empezarBtn: UIButton!
+    @IBOutlet weak var cardView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        inicializarIG()
     }
 
+    //Agregar estilo 
+    func inicializarIG() {
+        cardView.layer.cornerRadius = 12
+        cardView.layer.shadowOpacity = 0.4
+        cardView.layer.shadowColor = UIColor.black.cgColor
+        cardView.layer.shadowRadius = 12
+        cardView.layer.shadowOffset = .zero
+    }
+    
+    //Ocultar teclado
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        nombre.resignFirstResponder()
+    }
+    
+    @IBAction func empezarBtn(_ sender: UIButton) {
+        guard !nombre.text!.trimmingCharacters(in: .whitespaces).isEmpty else { return }
+        let controlador = storyboard?.instantiateViewController(withIdentifier: "gameScene") as! GameViewController
+        controlador.nombreJugador = nombre.text
+        controlador.modalTransitionStyle = .coverVertical
+        controlador.modalPresentationStyle = .fullScreen
+        self.present(controlador, animated: true)
+    }
+    
+    
+    //enviar el nombre a la 2 pantalla con un segue previamente creado desde el btn -> 2 pantalla
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destinoVC = segue.destination as? GameViewController {
+            destinoVC.nombreJugador = nombre.text
+        }
+    }
+    
+    //Enviar nombre validando que no este vacion
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        if identifier == "goToGame" {
+            if nombre.text!.trimmingCharacters(in: .whitespaces).isEmpty {
+                return false //Si el texto del TF coincide con algun espacio en blanco o estan vacios devuelve falso y no deja avanzar a la 2 pantalla
+            }
+        }
+        return true //Caso contrario, si hay algun caracter diferente a un espacio en blanco o vacio
+    }
 
 }
 
