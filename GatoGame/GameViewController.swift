a454cda171351b87c234f08ecf6e25aed844e8df
marcoalonso <marcoalonsorota@hotmail.com>
2022-01-19 13:40:33 -0600

    Empezando

---
+++ GatoGame/GameViewController.swift
@@ -0,0 +1,32 @@
+//
+//  GameViewController.swift
+//  GatoGame
+//
+//  Created by marco rodriguez on 19/01/22.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func regresarBtn(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
